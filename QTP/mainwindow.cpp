40ebcd4863b8795f9eea93de6f6a436021471b25
Maxime Alvarez <maxime.alvarez@etu.univ-tours.fr>
2018-02-16 17:17:55 +0100

    Appel du dialogue de création client

--- QTP/mainwindow.cpp
+++ QTP/mainwindow.cpp
@@ -1,6 +1,8 @@
 #include "mainwindow.h"
 #include "ui_mainwindow.h"
 
+#include "views/createclientdialog.h"
+
 MainWindow::MainWindow(QWidget *parent)
 	: QMainWindow(parent)
 	, ui(new Ui::MainWindow)
@@ -22,7 +24,8 @@ MainWindow::~MainWindow()
 
 void MainWindow::onCreateClientAction()
 {
-
+	CreateClientDialog *ccd = new CreateClientDialog(this);
+	ccd->exec();
 }
 
 void MainWindow::onCreateStaffAction()
