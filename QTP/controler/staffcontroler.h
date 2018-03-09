ca9a4844eeac58b76948b1fa502ccf83bd5d34f2
Maxime Alvarez <maxime.alvarez@etu.univ-tours.fr>
2018-03-09 11:31:08 +0100

    Ajout de commentaires

--- QTP/controler/staffcontroler.h
+++ QTP/controler/staffcontroler.h
@@ -5,12 +5,26 @@
 #include "views/createstaffdialog.h"
 #include <QString>
 
+/**
+ * @brief The StaffControler class
+ */
 class StaffControler
 {
-public:
-    StaffControler();
+	public:
+		/**
+		 * @brief StaffControler constructor
+		 */
+		StaffControler();
 
-    void createStaff(QString firstName, QString lastName, QString type, QString login, QString password);
+		/**
+		 * @brief createStaff create a new staff
+		 * @param firstName the first name of the staff
+		 * @param lastName the last name of the staff
+		 * @param type the type of the staff ("Banquier A", "Banquier B", ...)
+		 * @param login the login for the staff ONLY USED FOR type "Informaticien"
+		 * @param password the login for the staff ONLY USED FOR type "Informaticien"
+		 */
+		void createStaff(QString firstName, QString lastName, QString type, QString login, QString password);
 };
 
 #endif // STAFFCONTROLER_H
